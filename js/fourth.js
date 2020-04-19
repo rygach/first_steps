005c75d6f7ff9e61630f61fa91739e2cd203f3ad
rygach <rygacher@gmail.com>
2020-04-19 23:39:34 +0300

    some things

---
+++ js/fourth.js
@@ -0,0 +1,13 @@
+//let modalAnal = document.getElementById('modalAnal');
+let myBtn4 = document.getElementById('myBtn4');
+let close4 = document.getElementsByClassName('close')[2];
+
+
+myBtn4.onclick = function() {
+    //modalAnal.style.display = 'block';
+    console.log('it\'s working')
+}
+
+close3.onclick = function () {
+	modalAnal.style.display = 'none'; 
+}
